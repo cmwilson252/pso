3eacc4e5a26538df562c0947f00d45f5f4d0a95f
cmwilson252 <44687779+cmwilson252@users.noreply.github.com>
2019-02-19 22:10:57 -0600

    Start?
    First upload

---
+++ teamz/assets/js/quests.js
@@ -0,0 +1,209 @@
+var questListData = [
+    {
+        "name": "TTF Ultima",
+        "enabled": true
+    },
+    {
+        "name": "Brutal Baranz Blight ",
+        "enabled": true
+    },
+    {
+        "name": "MAS",
+        "enabled": true
+    },
+    {
+        "name": "AOL Cup - Maximum Attack -",
+        "enabled": true
+    },
+    {
+        "name": "MA1A",
+        "enabled": true
+    },
+    {
+        "name": "MA1B",
+        "enabled": true
+    },	
+    {
+        "name": "MA1C",
+        "enabled": true
+    },
+    {
+        "name": "MA1V2",
+        "enabled": true
+	},
+	{
+		"name": "MAU: Forest",
+		"enabled": true
+	},
+	{
+		"name": "MAU: Caves",
+		"enabled": true
+	},
+	{
+		"name": "MAU: Mines",
+		"enabled": true
+	},
+	{
+		"name": "MAU: Ruins",
+		"enabled": true
+	},
+	{
+		"name": "Christmas Fiasco I",
+		"enabled": true
+	},
+	{
+		"name": "Passion of the Blacksmith",
+		"enabled": true
+	},
+    {
+        "name": "MU1",
+        "enabled": true
+    },
+    {
+        "name": "MU2",
+        "enabled": true
+    },
+    {
+        "name": "MU3",
+        "enabled": true
+    },
+    {
+        "name": "MU4",
+        "enabled": true
+    },
+    {
+        "name": "TTF",
+        "enabled": true
+    },
+    {
+        "name": "MA2A",
+        "enabled": true
+    },
+    {
+        "name": "MA2B",
+        "enabled": true
+    },	
+    {
+        "name": "MA2C",
+        "enabled": true
+    },
+    {
+        "name": "MAS",
+        "enabled": true
+	},
+	{
+		"name": "MAU: Temple",
+		"enabled": true
+	},
+	{
+		"name": "MAU: Spaceship",
+		"enabled": true
+	},
+	{
+		"name": "MAU: CCA",
+		"enabled": true
+	},
+	{
+		"name": "MAU: Seabed",
+		"enabled": true
+	},
+	{
+		"name": "MAU: Tower",
+		"enabled": true
+	},
+	{
+		"name": "Christmas Fiasco II",
+		"enabled": true
+	},
+    {
+        "name": "MU1",
+        "enabled": true
+    },
+    {
+        "name": "MU2",
+        "enabled": true
+    },
+    {
+        "name": "MU3",
+        "enabled": true
+    },
+    {
+        "name": "MU4",
+        "enabled": true
+    },
+    {
+        "name": "MU5",
+        "enabled": true
+    },
+    {
+        "name": "ROCT",
+        "enabled": true
+    },
+    {
+        "name": "Gal Da Val Darkness",
+        "enabled": true
+    },	
+    {
+        "name": "Power Plant Plight",
+        "enabled": true
+    },
+    {
+        "name": "Cal's Clock Challenge",
+        "enabled": true
+    },
+    {
+        "name": "MA4A",
+        "enabled": true
+    },
+    {
+        "name": "MA4B",
+        "enabled": true
+    },
+    {
+        "name": "MA4C",
+        "enabled": true
+    },
+    {
+        "name": "MA4DMD",
+        "enabled": true
+	},
+	{
+		"name": "MAU: Desert",
+		"enabled": true
+	}, {
+		"name": "MAU: Wilds",
+		"enabled": true
+	},
+	{
+		"name": "Christmas Fiasco III",
+		"enabled": true
+	},
+    {
+        "name": "NMU1",
+        "enabled": true
+    },
+    {
+        "name": "NMU2",
+        "enabled": true
+    },
+    {
+        "name": "NMU3",
+        "enabled": true
+    },
+    {
+        "name": "NMU4",
+        "enabled": true
+    },
+    {
+        "name": "NMU5",
+        "enabled": true
+	},
+    {
+        "name": "PoD",
+        "enabled": true
+    },
+    {
+        "name": "RoW",
+        "enabled": true
+    }
+];
