686a3b06fb6a704bcc2ba0efe3ff32211ff5b47a
cmwilson252 <44687779+cmwilson252@users.noreply.github.com>
2019-03-10 15:31:39 -0500

    stuff

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,6 +8,27 @@ if ($('.ui.dropdown').length > 0) {
     $('.ui.dropdown').dropdown();
 }
 
+let Enums = {
+    GameModeTypes:{ 
+        Challenge: 0, 
+        Normal: 1  
+    },
+    NumberOfPlayers: {
+        OPM_No_PB:0,
+        OPM_PB:1,
+        _2P_No_PB:2,
+        _2P_PB:3,
+        _4P_No_PB:4,
+        _4P_PB:5
+    },
+    Servers: {
+        Ultima: 0,
+        _2014:1,
+        Gamecube:2,
+        Vanilla:3
+    }
+}
+
 // Remove
 function animateAutoHeight(element, duration) {
     if(typeof duration === undefined) {
