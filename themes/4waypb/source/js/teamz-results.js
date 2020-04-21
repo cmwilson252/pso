a0d42907a6848809a268d420298dc5738760f874
Soleil Rojas <soleilrojas@gmail.com>
2020-04-21 00:14:27 -0600

    teamz updates

--- themes/4waypb/source/js/teamz-results.js
+++ themes/4waypb/source/js/teamz-results.js
@@ -6,45 +6,98 @@ window.fourwaypb.teamz_results.ready = function() {
     let ready = false;
     let events = null;
     
+    function updateColumns() {
+        $('.cards-container').removeClass('one two three');
+        if (window.innerWidth <= 767) {
+            $('.cards-container').addClass('two');
+        } else if (window.innerWidth <= 991) {
+            $('.cards-container').addClass('two');
+        } else {
+            $('.cards-container').addClass('three');
+        }
+        
+        let cardContainerTitle = $('[data-column-wide]');
+        cardContainerTitle.each(function(index, el) {
+            $(el).removeClass($(el).data('column-wide'), $(el).data('column-wide-mobile'));
+        });
+        if (window.innerWidth <= 767) {
+            cardContainerTitle.each(function(index, el) {
+                $(el).addClass($(el).data('column-wide-mobile'));
+            });
+        } else {
+            cardContainerTitle.each(function(index, el) {
+                $(el).addClass($(el).data('column-wide'));
+            });
+        }
+    }
+    
     function createResult(event) {
         return $('<div/>', {
             'class': 'ui inverted segment',
         }).append(
             $('<div/>', {
-                'class': 'ui grid stacking three column',
+                'class': 'ui grid stacking',
             }).append(
                 $('<div/>', {
-                    'class': 'twelve wide column',
+                    'class': 'row',
                 }).append(
-                    $('<h2/>', {
-                        'text': event.quest.name,
-                    }),
-                ),
-                $('<div/>', {
-                    'class': 'two wide column',
-                }).append(
-                    $('<h3/>', {
-                        'text': event.duration,
-                    }),
-                ),
-                $('<div/>', {
-                    'class': 'two wide column',
-                }).append(
-                    $('<h4/>', {
-                        'text': event.date,
-                    }),
+                    $('<div/>', {
+                        'class': 'seven wide column',
+                        'data-column-wide': 'seven wide',
+                        'data-column-wide-mobile': 'eight wide',
+                    }).append(
+                        $('<h2/>', {
+                            'text': event.quest.name,
+                        }),
+                    ),
+                    $('<div/>', {
+                        'class': 'three wide column',
+                        'data-column-wide': 'three wide',
+                        'data-column-wide-mobile': 'eight wide',
+                    }).append(
+                        event.set_game == 0 ? null :
+                        $('<h5/>', {
+                            'text': 'Game #'+event.set_game,
+                        }),
+                    ),
+                    $('<div/>', {
+                        'class': 'three wide column',
+                        'data-column-wide': 'three wide',
+                        'data-column-wide-mobile': 'eight wide',
+                    }).append(
+                        $('<h4/>', {
+                            'text': event.duration,
+                        }),
+                    ),
+                    $('<div/>', {
+                        'class': 'three wide column',
+                        'data-column-wide': 'three wide',
+                        'data-column-wide-mobile': 'eight wide',
+                    }).append(
+                        $('<h4/>', {
+                            'text': event.date,
+                        }),
+                    ),
                 ),
             ),
             $('<div/>', {
-                'class': 'ui three stackable cards',
+                'class': 'ui three stackable cards cards-container',
             }).append(
                 $.map(event.teams, function(team) {
                     return $('<div/>', {
-                        'class': 'ui inverted card '+(team.winner ? 'winner' : ''),
+                        'class': 'ui inverted card '+(team.highlight ? 'highlight' : ''),
                     }).append(
                         $('<div/>', {
                             'class': 'content',
                         }).append(
+                            event.set_game == 0 ? null :
+                            $('<div/>', {
+                                'class': 'right floated meta',
+                            }).append(
+                                $('<span/>', {
+                                    'text': team.score.wins+' - '+team.score.losses,
+                                }),
+                            ),
                             $('<div/>', {
                                 'class': 'meta',
                             }).append(
@@ -62,20 +115,39 @@ window.fourwaypb.teamz_results.ready = function() {
                             $('<div/>', {
                                 'class': 'description',
                             }).append(
-                                $('<ul/>', {}).append(
-                                    $.map(team.players, function(player) {
-                                        return $('<li/>').append(
-                                            $('<a/>', {
-                                                'href': url_for('player?id='+player.id),
-                                                'text': player.name,
-                                            })
-                                        );
-                                    })
+                                $('<table/>', {
+                                    'class': 'ui inverted very basic small compact unstackable table',
+                                }).append(
+                                    $('<tbody/>', {}).append(
+                                        $.map(team.players, function(player) {
+                                            return $('<tr/>').append(
+                                                $('<td/>').append(
+                                                    $('<a/>', {
+                                                        'href': url_for('player?id='+player.id),
+                                                        'text': player.name,
+                                                        'class': ' '+(player.is_captain ? 'captain' : ''),
+                                                    })
+                                                ),
+                                                $('<td/>').append(
+                                                    $('<span/>', {
+                                                        'text': (player.class ? window.fourwaypb.classKeyToName(player.class) : 'N/A'),
+                                                    })
+                                                ),
+                                                $('<td/>').append(
+                                                    $('<a/>', {
+                                                        'href': player.video,
+                                                        'text': 'POV',
+                                                        'class': ' '+(player.video ? '' : 'visibility-hidden'),
+                                                    })
+                                                ),
+                                            );
+                                        }),
+                                    ),
                                 ),
                             ),
                         ),
                     );
-                })
+                }),
             ),
         );
     }
@@ -91,6 +163,11 @@ window.fourwaypb.teamz_results.ready = function() {
                 ];
             }),
         );
+        
+        $(window).on('resize', _.debounce(function () {
+            updateColumns();
+        }, 250));
+        updateColumns();
     };
     
     getJSON5(url_for('data/teamz.json'), (function(data) {
